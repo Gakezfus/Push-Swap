99e55f03f92f2cb5b7623adb08455ddb15f206e7
Elkan <elkanchoo@gmail.com>
2025-12-10 23:24:33 +0800

    Thinking

---
+++ push_swap.c
@@ -0,0 +1,19 @@
+
+#include <stdlib.h>
+
+typedef struct branch
+{
+	
+}
+
+int	main(int argc, char *argv)
+{
+	if (argc < 2)
+		return (1);
+	iterative_search(argc - 1, argv);
+}
+
+void	iterative_search(int count, char *list)
+{
+	
+}
