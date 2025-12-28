ba0467934b0b8d3389f4b87e7877fc2c6d7ad5db
Elkan <echoo@42mail.sutd.edu.sg>
2025-12-28 23:29:58 +0800

    indexes replacement OK so far...

--- post_processing.c
+++ post_processing.c
@@ -17,31 +17,32 @@
 void	update(int *path, int *dup, int c_index, int *p_index);
 int		math(int act);
 
+// indexes[0] represents the index of path, indexes[1] is used to search the
+// number of consecutive actions in a row, indexesp[2] is used to store the
+// value of the initial consecutive actions
 void	post_processing(int *path, int *dup, int int_len)
 {
-	static int	p_index = 0;
-	int			c_index;
-	int			row;
+	static int	indexes[3];
 
-	while (p_index < int_len)
+	while (indexes[0] < int_len)
 	{
-		row = 0;
-		c_index = 0;
-		if (5 <= path[p_index] && path[p_index] < 8)
+		indexes[2] = 0;
+		indexes[1] = 0;
+		if (5 <= path[indexes[0]] && path[indexes[0]] < 8)
 		{
-			while (path[p_index] == path[p_index + c_index])
-				c_index++;
-			row = c_index;
-			c_index = 0;
-			// When encountering 6665, c_index == 1, so 66 will be replace by 10, causing bug. 
-			while (c_index < row &&
-					(math(path[p_index]) == path[p_index + row + c_index]))
-				c_index++;
-			// c_index represents the number of "in a rows" that can be
+			while (path[indexes[0]] == path[indexes[0] + indexes[1]])
+				indexes[1]++;
+			indexes[2] = indexes[1];
+			indexes[1] = 0;
+			// When encountering 6665, indexes[1] == 1, so 66 will be replace by 10, causing bug. 
+			while (indexes[1] < indexes[2] &&
+					(math(path[indexes[0]]) == path[indexes[0] + indexes[2] + indexes[1]]))
+				indexes[1]++;
+			// indexes[1] represents the number of "in a rows" that can be
 			// replaced by rr or rrr. If 0, just copy over. Otherwise,
-			// add the appropriate elements to dup and move p_index accordingly
+			// add the appropriate elements to dup and move indexes[0] accordingly
 		}
-		update(path, dup, c_index, &p_index);
+		update(path, dup, indexes[1], &indexes[0]);
 	}
 	// for (int i = 0; i < int_len; i++)
 	// {
